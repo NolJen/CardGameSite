d4d01c5a862e648a895fefdeb62389a8feb79009
Dante Buhl <dbuhl8@gmail.com>
2023-05-15 18:07:20 -0700

    CSS/JS
    Added folders and brief files for css/js

---
+++ js/bckendjs.js
@@ -0,0 +1,10 @@
+// Script to run the backend of a game server
+
+/* Things to add:
+
+    game hosting
+    handing api calls
+    etc
+
+
+*/
---
+++ js/script.js
@@ -0,0 +1,8 @@
+// script for the front end of the website!
+
+/* Things to add: 
+    location calls,
+    scheduler calls,
+    timer?
+    
+*/
