3036f6310154822fa21d2e915a813b5857116a38
Dante Buhl <dbuhl8@gmail.com>
2023-05-24 18:52:54 -0700

    JS Updates
    Added server and API call structures to the backend and frontend files. Decorated the files with more detail

--- js/bckendjs.js
+++ js/bckendjs.js
@@ -7,4 +7,89 @@
     etc
 
 
+    Back end stuff! 
+    When a user clicks onto the online game page, 
+    they need to obtain a game id, and pick their character, 
+    after this, bots should be generated and calls to a ChatGPT
+     api should be made to instantialize characters. 
+
+    Return game id, instantialize
 */
+
+const request = require("request");
+const express = require('express');
+
+const app = express();
+
+const GPT_KEY = "";
+const Deck_KEY = "";
+const GGlCal_KEY = "AIzaSyA5fjHr2agoW93queC4T9jUYg1ay0FzP7o";
+
+var gameGen = 0;
+const PORTID = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Ready to receive game start');
+    console.log(`Game Ready to be Initialized`);
+
+    // possibly include a language conversion???
+});
+
+app.get('/start_game/:charid/:botNum', (req, res) => {
+
+    let charid = req.params.charid;
+    let botNum = req.params.botNum;
+    
+    // Call deck of cards API, initialize a deck or two
+
+    // Create Bots
+
+    // Decide turn order
+
+    
+    retString = JSON.stringify({ gameid : gameGen += 1, handid : playerPile})
+
+    res.send(retString)
+    console.log(retString)
+});
+
+app.get('/play_card/:handid/:cardid/:deckid', (req, res) => {
+
+    // add card to discard pile
+
+    // enforce card effect 
+
+    // queue bots turns or play another card
+
+});
+
+
+app.get('/update_map/', (req, res) => {
+
+    // google maps things
+
+});
+
+app.get('/add_to_schedule/:scheduleID', (req, res) => {
+
+    // Add event to your google calender
+
+});
+
+app.listen(PORTID, () => {
+    console.log(`Running on Port ID: ${PORTID}, and waiting!`)
+});
+
+
+function CardConverter(cardid, deckid, handid) {
+ 
+    // takes card id as given from player, and returns the corresponding
+    // deck of cards card id. A different verison of this function will run backwards. 
+
+}
+
+
+//Functions that run the game need to be added, either here or to the front end file
+
+
--- js/refrence.js
+++ js/refrence.js
@@ -1,7 +1,35 @@
-$(function () {
-    $("li").click(function (e) {
-        e.preventDefault();
-        $("li").removeClass("active");
-        $(this).addClass("active");
-    });
-  });
+
+
+let hand = [];
+var charID = hey; // This should be updated before the new game call
+var SERURL = ''; // This should call the new game function with a selected character already chosen!
+
+
+var xReq = new XMLHttpRequest();
+xReq.open("GET", `${SERURL}`);
+xReq.send();
+xReq.onload = function() {
+    const body = JSON.parse(xReq.responseText);
+
+    const gameID = body.gameID;
+    const charID = body.charID;
+    const pileID = body.pileID;
+
+    // Some code here that starts the game animation / game screen 
+
+    // Some code that makes the player draw their starting cards
+}
+
+// xReq for dealing cards and starting game (random order and who goes first)
+
+// xReq wait period for your turn!
+
+// xReq 
+
+// functions that run the game might need to be written here. 
+
+
+
+
+
+
